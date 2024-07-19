754253b766e532eed6ce4d520006aaf9474bdd85
harshmm21 <moradi21@uwindsor.ca>
2024-07-19 12:35:29 -0400

    ED-63 Integrate API to Modify Application Status(Withdraw, Accept or Reject)

--- src/components/LandApplications.js
+++ src/components/LandApplications.js
@@ -11,27 +11,60 @@ import LandAgreementForm from "./LandAgreementForm";
 import LandAgreementCard from "./LandAgreementCard";
 import FarmerProfile from "./FarmerProfile";
 
-
-
 const LandApplications = () => {
-
   const navigate = useNavigate();
   const [lands, setLands] = useState([]);
   const [applications, setApplications] = useState([]);
-  const [selectedLandId, setSelectedLandId] = useState(null);
   const [agreements, setAgreements] = useState([]);
+  const [selectedLandId, setSelectedLandId] = useState(null);
   const [filteredApplications, setFilteredApplications] = useState([]);
   const [farmersInfo, setfarmersInfo] = useState([]);
   const [activeTab, setActiveTab] = useState("lands");
+  const [isModalOpen, setIsModalOpen] = useState(false);
   const [openModal, setOpenModal] = useState(false);
   const [selectedLandDetails, setSelectedLandDetails] = useState(null);
   const [isTagVisible, setIsTagVisible] = useState(false);
+  const [showFarmerProfile, setShowFarmerProfile] = useState(false); // State to control FarmerProfile modal visibility
   const [selectedFarmerId, setSelectedFarmerId] = useState(null);
+  const [isConfirmationOpen, setIsConfirmationOpen] = useState(false);
   const [pendingApplicationId, setPendingApplicationId] = useState(null);
+
+  const promptIgnoreConfirmation = (appId) => {
+    setPendingApplicationId(appId);
+    setIsConfirmationOpen(true);
+  };
 
   const showNotification = (message) => {
     toast(message);
-  }
+  };
+
+  const [formData, setFormData] = useState({
+    landOwnerName: "",
+    farmerName: "",
+    landAddress: "",
+    agreementDuration: "",
+    durationType: "years",
+    decidedCrop: "",
+    facilitiesAndEquipment: "",
+    agreementDescription: "",
+  });
+
+  const activeTabClass = "bg-green-200";
+  const inactiveTabClass =
+    "hover:bg-green-100 text-gray-700 hover:text-gray-900";
+
+  let passedId;
+
+  const handleFarmerProfileOpen = (farmerId) => {
+    passedId = farmerId;
+    setSelectedFarmerId(farmerId);
+
+    setShowFarmerProfile(true);
+  };
+
+  const handleFarmerProfileClose = () => {
+    setShowFarmerProfile(false);
+  };
 
   const storedDBData = JSON.parse(localStorage.getItem("storedDBData"));
   let isLandOwner = false;
@@ -40,6 +73,7 @@ const LandApplications = () => {
   }
 
   const storedUserData = JSON.parse(localStorage.getItem("storedDBData"));
+
   useEffect(() => {
     const fetchApplications = async () => {
       const response = await axios.get(
@@ -51,6 +85,7 @@ const LandApplications = () => {
             (application) => application.landowner === storedUserData.id
           );
           setApplications(currentApplications);
+          // fetchExtendedUsers();
         } else {
           const currentApplications = response.data.filter(
             (application) => application.farmer === storedUserData.id
@@ -63,9 +98,7 @@ const LandApplications = () => {
     };
 
     const fetchAgreements = async () => {
-      const response = await axios.get(
-        "http://127.0.0.1:8000/api/agreements"
-      );
+      const response = await axios.get("http://127.0.0.1:8000/api/agreements");
       if (response.data && storedUserData) {
         if (isLandOwner) {
           const currentAgreements = response.data.filter(
@@ -85,7 +118,6 @@ const LandApplications = () => {
 
     fetchApplications();
     fetchAgreements();
-
   }, []);
 
   useEffect(() => {
@@ -102,12 +134,21 @@ const LandApplications = () => {
           console.log("fetched lands", storedUserData.user_name);
           setLands(filteredLands);
         } else {
+          // Get all land ids from land applications
           const landIdsInApplications = applications.map((app) => app.landid);
+
+          // Filter lands that are in land applications
           const filteredLands = response.data.filter((land) =>
             landIdsInApplications.includes(land.id)
           );
           setLands(filteredLands);
         }
+
+        // // Filter JSON B based on the farmer ID matching a variable called userId
+        // const filteredFarmerApplications = jsonDataB.filter(item => item.farmer === storedDBData.id);
+
+        // // Filter JSON A based on matching IDs from JSON B
+        // const filteredJsonA = jsonDataA.filter(itemA => jsonDataB.some(itemB => itemA.id === itemB.landid));
       } else {
         setLands(response.data);
       }
@@ -124,6 +165,7 @@ const LandApplications = () => {
 
     if (isLandOwner) {
       const farmerIds = new Set(applications.map((app) => app.farmer));
+
       const farmersInfo = extendedUsers
         .filter((user) => farmerIds.has(user.id))
         .map((user) => ({ userId: user.id, username: user.user_name }));
@@ -137,7 +179,7 @@ const LandApplications = () => {
         .filter((user) => landownerIds.has(user.id))
         .map((user) => ({ userId: user.id, username: user.user_name }));
 
-      console.log("found landow", farmersInfo);
+      console.log("found land", farmersInfo);
 
       setfarmersInfo(farmersInfo);
     }
@@ -153,9 +195,8 @@ const LandApplications = () => {
   const [landOwnerId, setLandOwnerId] = useState();
   const [farmerId, setFarmerId] = useState();
   const [landId, setLandId] = useState();
-  const [showFarmerProfile, setShowFarmerProfile] = useState(false);
-  const [isLoading, setIsLoading] = useState(true);
-
+  const [landApplication, setLandApplication] = useState();
+  const [isLoading, setIsLoading] = useState(true); // Add a loading state
 
   const fetchFormData = ({ appId, landowner, farmer, landid }) => {
     setLandOwnerId(landowner);
@@ -175,12 +216,11 @@ const LandApplications = () => {
   };
 
   useEffect(() => {
-    // This useEffect runs whenever selectedLandId changes.
     const newFilteredApplications = selectedLandId
       ? applications.filter((app) => app.landid === selectedLandId)
       : applications;
     setFilteredApplications(newFilteredApplications);
-  }, [selectedLandId, applications]); // Depend on both selectedLandId and applications to re-filter when either changes
+  }, [selectedLandId, applications]);
 
   const handleLandClick = (land) => {
     setSelectedLandId(land.id);
@@ -192,59 +232,68 @@ const LandApplications = () => {
     setIsTagVisible(false);
   };
 
-  const handleAccept = (id, landowner, farmer, landid) => {
-    fetchFormData({ id, landowner, farmer, landid });
-    console.log("Accepted application with ID:", id);
+  const handleAccept = (landApplication) => {
+    setLandApplication(landApplication);
     setOpenModal(true);
   };
 
-  const handleIgnore = (appId) => {
-    
+  const updateLandApplicationStatus = async (appId, status) => {
+    try {
+      const response = await axios.put(
+        `http://127.0.0.1:8000/api/landapplications/${appId}/status`,
+        { status: status }
+      );
+      console.log(response.data);
+    } catch (error) {
+      console.error("error", error);
+    }
   };
+
+  const handleIgnore = (appId) => {
+    console.log("Ignored application with ID:", appId);
+    setOpenModal(false);
+    const updatedFilteredApplications = filteredApplications.filter(
+      (application) => application.id !== appId
+    );
+    updateLandApplicationStatus(appId, "Rejected");
+    setFilteredApplications(updatedFilteredApplications);
+    setIsConfirmationOpen(false);
+    showNotification("Application rejected.");
+  };
+
   useEffect(() => {
     setActiveTab("applications");
   }, []);
-
-  let passedId;
-
-  const handleFarmerProfileOpen = (farmerId) => {
-    passedId = farmerId;
-    setSelectedFarmerId(farmerId);
-
-    setShowFarmerProfile(true);
-  };
-
-  const handleFarmerProfileClose = () => {
-    setShowFarmerProfile(false);
-  };
-
 
   return (
     <div className="flex flex-col h-screen">
       <header className="flex justify-between items-center text-lg font-semibold bg-gray-300 shadow-md">
         <button
-          className={`flex-grow text-center py-2 rounded-t-lg transition-colors duration-300 ${activeTab === "applications"
-            ? "bg-gray-400 text-white"
-            : "text-gray-800 hover:bg-gray-200"
-            }`}
+          className={`flex-grow text-center py-2 rounded-t-lg transition-colors duration-300 ${
+            activeTab === "applications"
+              ? "bg-gray-400 text-white"
+              : "text-gray-800 hover:bg-gray-200"
+          }`}
           onClick={() => setActiveTab("applications")}
         >
           Applications
         </button>
         <button
-          className={`flex-grow text-center py-2 rounded-t-lg transition-colors duration-300 ${activeTab === "agreements"
-            ? "bg-gray-400 text-white"
-            : "text-gray-800 hover:bg-gray-200"
-            }`}
+          className={`flex-grow text-center py-2 rounded-t-lg transition-colors duration-300 ${
+            activeTab === "agreements"
+              ? "bg-gray-400 text-white"
+              : "text-gray-800 hover:bg-gray-200"
+          }`}
           onClick={() => setActiveTab("agreements")}
         >
           Agreements
         </button>
         <button
-          className={`flex-grow text-center py-2 rounded-t-lg transition-colors duration-300 ${activeTab === "chat"
-            ? "bg-gray-400 text-white"
-            : "text-gray-800 hover:bg-gray-200"
-            }`}
+          className={`flex-grow text-center py-2 rounded-t-lg transition-colors duration-300 ${
+            activeTab === "chat"
+              ? "bg-gray-400 text-white"
+              : "text-gray-800 hover:bg-gray-200"
+          }`}
           onClick={() => setActiveTab("chat")}
         >
           Chat
@@ -262,7 +311,7 @@ const LandApplications = () => {
             <div className="flex flex-row">
               <div className="w-3/4 p-4">
                 {isLoading ? (
-                  <div>Loading lands...</div> 
+                  <div>Loading lands...</div>
                 ) : (
                   <LandCard lands={lands} onLandClick={handleLandClick} />
                 )}
@@ -283,7 +332,10 @@ const LandApplications = () => {
         {activeTab === "agreements" && (
           <div>
             <div className="w-full" style={{ minHeight: "100vh" }}>
-              <LandAgreementCard lands={agreements} onLandClick={handleLandClick} />
+              <LandAgreementCard
+                lands={agreements}
+                onLandClick={handleLandClick}
+              />
             </div>
           </div>
         )}
@@ -295,6 +347,21 @@ const LandApplications = () => {
         )}
       </main>
 
+      <Modal
+        show={showFarmerProfile}
+        onClose={handleFarmerProfileClose}
+        size="4xl"
+      >
+        <Modal.Header>
+          <h2 className="text-2xl font-semibold mb-5 text-gray-900">
+            Farmer Profile
+          </h2>
+        </Modal.Header>
+        <Modal.Body>
+          <FarmerProfile farmerId={selectedFarmerId} />
+        </Modal.Body>
+      </Modal>
+
       {openModal && (
         <Modal show={openModal} onClose={() => setOpenModal(false)} size="4xl">
           <Modal.Header>
@@ -305,16 +372,21 @@ const LandApplications = () => {
           <Modal.Body>
             <LandAgreementForm
               preFormData={{
-                landOwnerId,
-                farmerId,
-                landId,
-                landOwnerName,
-                farmerName,
-                landAddress,
+                landApplication,
               }}
             />
           </Modal.Body>
         </Modal>
+      )}
+
+      {isConfirmationOpen && (
+        <div className="confirmation-dialog">
+          <p>Are you sure you want to reject this application?</p>
+          <button onClick={() => handleIgnore(pendingApplicationId)}>
+            Confirm
+          </button>
+          <button onClick={() => setIsConfirmationOpen(false)}>Cancel</button>
+        </div>
       )}
 
       <ToastContainer position="top-right" />
